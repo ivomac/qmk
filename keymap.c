4e8c1af84c971c71b6e09170b3c49ef8572b211d
Ivo Maceira <ivomaceira@gmail.com>
2023-11-17 16:21:30 +0100

    update colemak & base qwerty

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@
 
 enum planck_layers {
 	L0_QWERTY,
-	L0,
+	L0_COLEMAK,
 	L1,
 	L2,
 	L3,
@@ -24,32 +24,7 @@ enum planck_layers {
 };
 
 enum custom_keycodes {
-	M_ACUTE = SAFE_RANGE,
-	M_DEADACUTE,
-	M_DEADGREEK,
-	M_GRAVE,
-	M_DOT,
-	M_DEADDOT,
-	M_LDOT,
-	M_TIMES,
-	M_BAR,
-	M_LBAR,
-	M_DEADBAR,
-	M_DEADGRAVE,
-	M_CIRCUM,
-	M_TILDE,
-	M_CEDILLA,
-	M_SCOLON,
-	M_UMLAUT,
-	M_NBSP,
-	M_GLEFT,
-	M_GRIGHT,
-	M_DEGREE,
-	M_MASC,
-	M_FEMI,
-	M_DQUOTELEFT,
-	M_DQUOTERIGHT,
-	M_DASH,
+	M_BASETG = SAFE_RANGE,
 	M_F1,
 	M_F2,
 	M_F3,
@@ -60,54 +35,71 @@ enum custom_keycodes {
 	M_F8,
 	M_F9,
 	M_F10,
-	M_F11,
-	M_F12,
+	M_DEADACUTE,
+	M_DEADGRAVE,
+	M_DEADDQUOTE,
+	M_DEADGREEK,
+	M_DEADDOT,
+	M_DEADBAR,
+	M_DEADCIRCUM,
+	M_DEADTILDE,
+	M_DEADCEDILLA,
+	M_DEADUMLAUT,
+	M_DEADSLASH,
+	M_DEADUNDERLINE,
+	M_APPROX,
+	M_DEF,
+	M_PM,
+	M_TIMES,
+	M_CDOT,
+	M_BULLET,
+	M_SCOLON,
+	M_DASH,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[L0_QWERTY] = LAYOUT_planck_mit(
-			KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,      KC_Y,          KC_U,         KC_I,    KC_O,    KC_P,     KC_BSPC,
-			KC_SLASH, KC_A,    KC_S,    KC_D,    KC_F,  KC_G,      KC_M,          KC_H,         KC_J,    KC_K,    KC_L,     KC_MINUS,
-			KC_ESC,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,      KC_N,          KC_BACKSLASH, KC_COMM, KC_DOT,  KC_QUOTE, KC_ENT,
-			KC_LEFT,  KC_DOWN, KC_LALT, KC_LCTL, LM(L1, MOD_LSFT), /***/  KC_SPC, MO(L2),       KC_LGUI, KC_RALT, KC_UP,    KC_RGHT
+			KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,            KC_T, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,  KC_BSPC,
+			KC_SLASH, KC_A,    KC_S,     KC_D,     KC_F,            KC_G, KC_M,   KC_H,   KC_J,    KC_K,    KC_L,  KC_BACKSLASH,
+			KC_ESC,   KC_Z,    KC_QUOTE, KC_MINUS, KC_V,            KC_C, KC_B,   KC_N,   KC_COMM, KC_DOT,  KC_X,  KC_ENT,
+			KC_LEFT,  KC_DOWN, KC_LALT,  KC_LCTL,  LM(L1,MOD_LSFT), /***/ KC_SPC, MO(L2), KC_LGUI, KC_RALT, KC_UP, KC_RGHT
 			),
 
-	[L0] = LAYOUT_planck_mit(
-			KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,  KC_B,      KC_J,          KC_L,   KC_U,    KC_Y,    KC_BACKSLASH, KC_BSPC,
-			KC_SLASH, KC_A,    KC_R,    KC_S,    KC_T,  KC_G,      KC_M,          KC_N,   KC_E,    KC_I,    KC_O,         KC_MINUS,
-			KC_ESC,   KC_Z,    KC_X,    KC_C,    KC_D,  KC_V,      KC_K,          KC_H,   KC_COMM, KC_DOT,  KC_QUOTE,     KC_ENT,
-			KC_LEFT,  KC_DOWN, KC_LALT, KC_LCTL, LM(L1, MOD_LSFT), /***/  KC_SPC, MO(L2), KC_LGUI, KC_RALT, KC_UP,        KC_RGHT
+	[L0_COLEMAK] = LAYOUT_planck_mit(
+			KC_TAB,   KC_Q,    KC_W,     KC_F,     KC_P,            KC_X, KC_J,   KC_L,   KC_U,    KC_Y,    KC_Z,  KC_BSPC,
+			KC_SLASH, KC_A,    KC_R,     KC_S,     KC_T,            KC_G, KC_M,   KC_N,   KC_E,    KC_I,    KC_O,  KC_BACKSLASH,
+			KC_ESC,   KC_V,    KC_QUOTE, KC_MINUS, KC_D,            KC_C, KC_B,   KC_H,   KC_COMM, KC_DOT,  KC_K,  KC_ENT,
+			KC_LEFT,  KC_DOWN, KC_LALT,  KC_LCTL,  LM(L1,MOD_LSFT), /***/ KC_SPC, MO(L2), KC_LGUI, KC_RALT, KC_UP, KC_RGHT
 			),
 
 	[L1] = LAYOUT_planck_mit(
-			_______, _______, _______, _______, _______, _______,          _______, _______, _______,   _______,      _______, _______,
-			_______, _______, _______, _______, _______, _______,          _______, _______, _______,   _______,      _______, _______,
-			_______, _______, _______, _______, _______, _______,          _______, _______, M_SCOLON,  KC_SEMICOLON, _______, _______,
-			_______, _______, _______, _______, _______, /***/    _______, _______, _______, TG(MOUSE), _______,      _______
+			_______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,      _______, _______,
+			_______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,      _______, _______,
+			_______, _______, _______, _______, _______, _______, _______, _______, M_SCOLON, KC_SEMICOLON, _______, _______,
+			_______, _______, _______, _______, _______, /***/    _______, _______, _______,  _______,      _______, _______
 			),
 
 	[L2] = LAYOUT_planck_mit(
-			KC_PSCR,    KC_0,          KC_1,       KC_2,           KC_3,          KC_4,               KC_5,           KC_6,       KC_7,           KC_8,         KC_9,       KC_DEL,
-			LSFT(KC_1), LSFT(KC_LBRC), LSFT(KC_9), LSFT(KC_0),     LSFT(KC_RBRC), LSFT(KC_3),         LSFT(KC_8),     LSFT(KC_6), KC_LBRC,        KC_RBRC,      LSFT(KC_4), COMPOSE,
-			KC_PGUP,    LSFT(KC_2),    LSFT(KC_5), LSFT(KC_EQUAL), KC_EQUAL,      M_ACUTE,            LSFT(KC_GRAVE), LSFT(KC_7), LSFT(KC_COMMA), LSFT(KC_DOT), KC_GRAVE,   KC_PGDN,
-			_______,    _______,       _______,    _______,        _______,       /***/       M_NBSP, _______,        _______,    _______,        _______,      _______
+			_______,    KC_1,          KC_2,       KC_3,           KC_4,          KC_5,                KC_6,       KC_7,       KC_8,           KC_9,         KC_0,       _______,
+			LSFT(KC_1), LSFT(KC_LBRC), LSFT(KC_9), LSFT(KC_0),     LSFT(KC_RBRC), LSFT(KC_3),          LSFT(KC_8), LSFT(KC_6), KC_LBRC,        KC_RBRC,      LSFT(KC_4), LSFT(KC_5),
+			_______,    XXXXXXX,       LSFT(KC_7), LSFT(KC_EQUAL), KC_EQUAL,      LSFT(KC_GRAVE),      LSFT(KC_2), KC_GRAVE,   LSFT(KC_COMMA), LSFT(KC_DOT), _______,    _______,
+			_______,    _______,       _______,    _______,        _______,       /***/       _______, _______,    _______,    _______,        _______,      _______
 			),
 
 
 	[L3] = LAYOUT_planck_mit(
-			KC_SYRQ, KC_BRK,  M_F1,          M_F2,    M_F3,    M_F4,              M_F5,    M_F6,     M_F7,      M_F8,      M_F9,        KC_INS,
-			QK_LOCK, KC_LEFT, KC_DOWN,       KC_UP,   KC_RGHT, M_DEADGREEK,       M_TIMES, M_CIRCUM, M_DOT,     M_DEADDOT, M_LDOT,      M_DEADBAR,
-			KC_HOME, M_LBAR,  DF(L0_QWERTY), DF(L0),  XXXXXXX, M_DEADACUTE,       M_TILDE, XXXXXXX,  M_CEDILLA, M_UMLAUT,  M_DEADGRAVE, KC_END,
-			QK_BOOT, XXXXXXX, XXXXXXX,       XXXXXXX, _______, /***/     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,   QK_BOOTLOADER
+			_______, M_F1,         M_F2,        M_F3,        M_F4,     M_F5,         M_F6,        M_F7,      M_F8,          M_F9,         M_F10,       KC_DEL,
+			COMPOSE, M_PM,      M_DASH,     M_CDOT,      M_BULLET, M_DEADGREEK,  M_TIMES,     M_DEADCIRCUM, M_DEF,         M_DEADDOT,    M_DEADSLASH,        KC_INS,
+			_______, XXXXXXX, M_DEADACUTE, M_DEADBAR, M_APPROX, M_DEADTILDE, XXXXXXX, M_DEADGRAVE,    M_DEADCEDILLA, M_DEADUMLAUT, XXXXXXX, _______,
+			KC_PSCR, KC_SYRQ,      XXXXXXX,     XXXXXXX,     _______,  /***/         TG(MOUSE),   _______,   XXXXXXX,       XXXXXXX,      M_BASETG,    QK_BOOTLOADER
 			),
 
-
 	[MOUSE] = LAYOUT_planck_mit(
-			_______, KC_KP_0,    KC_KP_1,    KC_KP_2,    KC_KP_3,       KC_KP_4,          KC_KP_5, KC_KP_6,       KC_KP_7,     KC_KP_8,  KC_KP_9,     _______,
-			XXXXXXX, KC_MS_BTN4, KC_MS_BTN3, KC_MS_BTN1, KC_MS_BTN2,    XXXXXXX,          XXXXXXX, KC_MS_LEFT,    KC_MS_DOWN,  KC_MS_UP, KC_MS_RIGHT, XXXXXXX,
-			_______, KC_MS_BTN8, KC_MS_BTN7, KC_MS_BTN6, KC_MS_BTN5,    XXXXXXX,          XXXXXXX, KC_MS_WH_DOWN, KC_MS_WH_UP, XXXXXXX,  XXXXXXX,     _______,
-			_______, _______,    _______,    _______,    KC_LEFT_SHIFT, /***/    _______, XXXXXXX, _______,       TG(MOUSE),   _______,  _______
+			_______, _______,    KC_MS_WH_DOWN, KC_MS_WH_UP, _______,       _______, _______, _______,    _______,    _______,  _______,     _______,
+			XXXXXXX, KC_MS_BTN4, KC_MS_BTN3,    KC_MS_BTN1,  KC_MS_BTN2,    XXXXXXX, XXXXXXX, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, XXXXXXX,
+			_______, KC_MS_BTN8, KC_MS_BTN7,    KC_MS_BTN6,  KC_MS_BTN5,    XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,  XXXXXXX,     _______,
+			_______, _______,    _______,       _______,     KC_LEFT_SHIFT, /***/    _______, TG(MOUSE),  _______,    _______,  _______,     _______
 			),
 
 };
@@ -173,12 +165,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_ACUTE:
+		case M_F10:
 			if (record->event.pressed) {
-				unregister_code(KC_LSFT);
-				tapkey(COMPOSE);
-				send_string("''");
-				register_code(KC_LSFT);
+				tapkey_noshift(KC_F10);
 			}
 			return false;
 			break;
@@ -196,6 +185,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
 				send_string("'");
+				register_code(KC_LSFT);
+			}
+			return false;
+			break;
+		case M_DEADDQUOTE:
+			if (record->event.pressed) {
+				unregister_code(KC_LSFT);
+				tapkey(COMPOSE);
+				send_string("\"");
 				register_code(KC_LSFT);
 			}
 			return false;
@@ -218,15 +216,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_LBAR:
-			if (record->event.pressed) {
-				unregister_code(KC_LSFT);
-				tapkey(COMPOSE);
-				send_string("---");
-				register_code(KC_LSFT);
-			}
-			return false;
-			break;
 		case M_DEADDOT:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
@@ -236,7 +225,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_DOT:
+		case M_PM:
+			if (record->event.pressed) {
+				unregister_code(KC_LSFT);
+				tapkey(COMPOSE);
+				send_string("+-");
+				register_code(KC_LSFT);
+			}
+			return false;
+			break;
+		case M_DEF:
+			if (record->event.pressed) {
+				unregister_code(KC_LSFT);
+				tapkey(COMPOSE);
+				send_string("=_");
+				register_code(KC_LSFT);
+			}
+			return false;
+			break;
+		case M_APPROX:
+			if (record->event.pressed) {
+				unregister_code(KC_LSFT);
+				tapkey(COMPOSE);
+				send_string("~~");
+				register_code(KC_LSFT);
+			}
+			return false;
+			break;
+		case M_CDOT:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
@@ -245,11 +261,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_LDOT:
+		case M_BULLET:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
-				send_string("=.");
+				send_string("o.");
 				register_code(KC_LSFT);
 			}
 			return false;
@@ -263,7 +279,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_TILDE:
+		case M_DEADTILDE:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
@@ -272,7 +288,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_CIRCUM:
+		case M_DEADCIRCUM:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
@@ -281,7 +297,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_CEDILLA:
+		case M_DEADCEDILLA:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
@@ -298,16 +314,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
-		case M_NBSP:
+		case M_DEADUNDERLINE:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
-				send_string("  ");
+				send_string("/");
 				register_code(KC_LSFT);
 			}
 			return false;
 			break;
-		case M_UMLAUT:
+		case M_DEADSLASH:
+			if (record->event.pressed) {
+				unregister_code(KC_LSFT);
+				tapkey(COMPOSE);
+				send_string("/");
+				register_code(KC_LSFT);
+			}
+			return false;
+			break;
+		case M_DEADUMLAUT:
 			if (record->event.pressed) {
 				unregister_code(KC_LSFT);
 				tapkey(COMPOSE);
@@ -325,7 +350,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 			break;
+		case M_BASETG:
+			if (record->event.pressed) {
+				default_layer_xor(1|2);
+			}
+			return false;
+			break;
 	}
 	return true;
 }
+
 
