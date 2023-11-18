886c5cb9bed8bf2e63ee8e43650f22ed2023b9e2
Ivo Maceira <ivomaceira@gmail.com>
2023-11-18 10:19:30 +0100

    revert changes to qwerty layer

--- keymap.c
+++ keymap.c
@@ -60,10 +60,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[L0_QWERTY] = LAYOUT_planck_mit(
-			KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,            KC_T, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,  KC_BSPC,
-			KC_SLASH, KC_A,    KC_S,     KC_D,     KC_F,            KC_G, KC_M,   KC_H,   KC_J,    KC_K,    KC_L,  KC_BACKSLASH,
-			KC_ESC,   KC_Z,    KC_QUOTE, KC_MINUS, KC_V,            KC_C, KC_B,   KC_N,   KC_COMM, KC_DOT,  KC_X,  KC_ENT,
-			KC_LEFT,  KC_DOWN, KC_LALT,  KC_LCTL,  LM(L1,MOD_LSFT), /***/ KC_SPC, MO(L2), KC_LGUI, KC_RALT, KC_UP, KC_RGHT
+			KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,            KC_T, KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     KC_BSPC,
+			KC_SLASH, KC_A,    KC_S,    KC_D,    KC_F,            KC_G, KC_M,   KC_H,     KC_J,    KC_K,    KC_L,     KC_BACKSLASH,
+			KC_ESC,   KC_Z,    KC_X,    KC_C,    KC_V,            KC_B, KC_N,   KC_MINUS, KC_COMM, KC_DOT,  KC_QUOTE, KC_ENT,
+			KC_LEFT,  KC_DOWN, KC_LALT, KC_LCTL, LM(L1,MOD_LSFT), /***/ KC_SPC, MO(L2),   KC_LGUI, KC_RALT, KC_UP,    KC_RGHT
 			),
 
 	[L0_COLEMAK] = LAYOUT_planck_mit(
